45d406d88786ea0cfc5b34e840b8565c6553bc42
ZionBlade <wernerct@mail.uc.edu>
2022-09-16 11:34:14 -0400

    commit finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int itemPrice = 5;
+        double salesTax;
+        double total;
+
+        salesTax = itemPrice * 0.05;
+        total = itemPrice + salesTax;
+
+        System.out.println("The price of the item is " + itemPrice + " and the sales tax is " + salesTax);
+        System.out.println("The total cost of the item is " + total);
     }
 }
